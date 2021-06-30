701d2da12f286dc3beaa9adda9550b4c3bdcd693
Olivier-Preaux <olivier.prx@gmail.com>
2021-06-30 03:10:03 +0200

    feat: bootstrap/css

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,9 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+    
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
